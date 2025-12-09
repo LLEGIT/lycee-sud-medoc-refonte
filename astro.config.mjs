1af5c03faeebcf78a5ddbcd6d59d89dafe3c9423
theo.gillet <theo.gillet@ekino.com>
2025-12-09 14:47:06 +0100

    fix: assets copied in /src

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,8 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://LLEGIT.github.io',
+  base: '/lycee-sud-medoc-refonte',
   vite: {
     plugins: [tailwindcss()],
   },
