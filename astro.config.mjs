81ee3dd744d7643ce522fce153bb1e0cbe668d59
theo.gillet <theo.gillet@ekino.com>
2025-12-11 10:50:25 +0100

    revert 22cdcc528316a9ffd7f2edb13ad709a4ad9dc2de

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,12 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
+import { defineConfig } from 'astro/config';
 
+// https://astro.build/config
 export default defineConfig({
   site: 'https://LLEGIT.github.io',
   base: '/lycee-sud-medoc-refonte',
   vite: {
     plugins: [tailwindcss()],
-    define: {
-      'import.meta.env.STRAPI_URL': JSON.stringify(process.env.STRAPI_URL || ''),
-      'import.meta.env.STRAPI_TOKEN': JSON.stringify(process.env.STRAPI_TOKEN || ''),
-    }
   },
 });
